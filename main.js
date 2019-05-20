52677bd8269535f57e9b375c885b8c598426f82f
piercecollazo <piercecollazo@gmail.com>
2019-05-20 15:29:48 -0400

    Code cleaned up and edge cases resolved. Project goals complete

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 let storedNum = 'banana'
 let displayNum = ''
 let currentAlg = ''
+
+// Array for handling numbers and decimal buttons
 const numPad = [document.querySelector("#num0"),document.querySelector("#num1"),document.querySelector("#num2"),document.querySelector("#num3"),document.querySelector("#num4"),document.querySelector("#num5"),document.querySelector("#num6"),document.querySelector("#num7"),document.querySelector("#num8"), document.querySelector("#num9"), document.querySelector("#decimal")]
+
+// Array for handling operators
+const operPad = [document.querySelector("#add"), document.querySelector("#subtract"), document.querySelector("#multiply"), document.querySelector("#divide")]
+
 window.onload = init();
 
 // init function for giving basic button responses
 function init(){
-    document.querySelector("#add").addEventListener("click", operator);
-
-    document.querySelector("#subtract").addEventListener("click", operator)
-
-    document.querySelector("#multiply").addEventListener("click", operator)
-
-    document.querySelector("#divide").addEventListener("click", operator)
+    operPad.forEach((i)=> i.addEventListener('click', operator))
     
     document.querySelector("#clear").addEventListener("click", clear)
 
@@ -73,6 +73,7 @@ function enter(){
             storedNum += parseFloat(displayNum)
             result.textContent = storedNum
             displayNum = storedNum.toString()
+            currentAlg = ''
         break;
 
         case '-':
@@ -82,6 +83,7 @@ function enter(){
                 storedNum -= parseFloat(displayNum)
                 result.textContent = storedNum
                 displayNum = storedNum.toString()
+                currentAlg = ''
             }
         break;
 
@@ -92,6 +94,7 @@ function enter(){
                 storedNum *= parseFloat(displayNum)
                 result.textContent = storedNum
                 displayNum = storedNum.toString()
+                currentAlg = ''
             }
         break;
 
@@ -102,6 +105,7 @@ function enter(){
                 storedNum /= parseFloat(displayNum)
                 result.textContent = storedNum
                 displayNum = storedNum.toString()
+                currentAlg = ''
             }
         break;
 
@@ -109,5 +113,6 @@ function enter(){
             storedNum = parseFloat(displayNum)
             result.textContent = storedNum
             displayNum = storedNum.toString()
+            currentAlg = ''
     }
 }
